a3d61c952a6c01ceaacc27bc1609c6f9b8dec6be
TormentedTraveler <esyashaba@gmail.com>
2025-03-19 00:24:47 +0600

    init

---
+++ src/main/java/com/esen/java_kanban_rework/JavaKanbanReworkApplication.java
@@ -0,0 +1,13 @@
+package com.esen.java_kanban_rework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKanbanReworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaKanbanReworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esen/java_kanban_rework/JavaKanbanReworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esen.java_kanban_rework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKanbanReworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
